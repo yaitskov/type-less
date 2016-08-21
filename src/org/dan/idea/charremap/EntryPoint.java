04c94369c4852e93ca1aeb4e44472963fe65ba30
Daneel Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2016-08-21 18:18:13 +0200

    init slf4j

--- src/org/dan/idea/charremap/EntryPoint.java
+++ src/org/dan/idea/charremap/EntryPoint.java
@@ -5,15 +5,19 @@ import java.util.Map;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.diagnostic.Logger;
+
 import com.intellij.openapi.editor.actionSystem.EditorActionManager;
 import com.intellij.openapi.editor.actionSystem.TypedAction;
+import com.intellij.openapi.editor.actionSystem.TypedActionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class EntryPoint extends AnAction {
-    private static final Logger logger = Logger.getInstance(EntryPoint.class);
+    private static final Logger logger = LoggerFactory.getLogger(CharRemapTypeHandler.class);
 
     static {
+        com.intellij.openapi.diagnostic.Logger.getInstance(EntryPoint.class).info("Init logging");
         logger.info("Init Type-Less");
         final EditorActionManager actionManager = EditorActionManager.getInstance();
         final TypedAction typedAction = actionManager.getTypedAction();
