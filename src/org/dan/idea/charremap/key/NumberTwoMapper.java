b1c7e36f9e6aeb6aae9a67d074b0c7e2c6c9ac63
Daneel Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2016-08-26 20:39:56 +0200

    most cases for 2=>@

--- src/org/dan/idea/charremap/key/NumberTwoMapper.java
+++ src/org/dan/idea/charremap/key/NumberTwoMapper.java
@@ -1,7 +1,11 @@
 package org.dan.idea.charremap.key;
 
 import static com.intellij.psi.impl.java.stubs.JavaStubElementTypes.JAVA_FILE;
+import static com.intellij.psi.impl.java.stubs.JavaStubElementTypes.METHOD;
+import static com.intellij.psi.impl.java.stubs.JavaStubElementTypes.PARAMETER;
+import static com.intellij.psi.impl.java.stubs.JavaStubElementTypes.PARAMETER_LIST;
 import static com.intellij.psi.impl.source.tree.JavaElementType.CLASS;
+import static com.intellij.psi.impl.source.tree.JavaElementType.FIELD;
 import static com.intellij.psi.impl.source.tree.JavaElementType.MODIFIER_LIST;
 import static java.util.Optional.of;
 import static org.dan.idea.charremap.composite.Any.any;
@@ -9,6 +13,8 @@ import static org.dan.idea.charremap.composite.Maybe.maybe;
 import static org.dan.idea.charremap.composite.Not.not;
 import static org.dan.idea.charremap.composite.One.WS;
 import static org.dan.idea.charremap.composite.One.one;
+import static org.dan.idea.charremap.composite.Or.or;
+import static org.dan.idea.charremap.composite.Plus.plus;
 import static org.dan.idea.charremap.composite.PrevChar.prevChar;
 import static org.dan.idea.charremap.composite.Seq.seq;
 
@@ -44,7 +50,12 @@ public class NumberTwoMapper implements Mapper {
                 types(elementAt.getNode().getTreePrev()));
         String docText = ce.editor.getDocument().getText();
         MatcherState state = new MatcherState(docText, offset, elementAt.getNode());
-        Matcher m = seq(WS, maybe(MODIFIER_LIST), any(CLASS), one(JAVA_FILE),
+        Matcher m = seq(WS,
+                or(
+                        seq(one(FIELD), plus(CLASS)),
+                        seq(maybe(PARAMETER), one(PARAMETER_LIST), one(METHOD), plus(CLASS)),
+                        seq(maybe(MODIFIER_LIST), any(CLASS))),
+                one(JAVA_FILE),
                 not(prevChar(Character::isJavaIdentifierPart)));
         if (m.test(state)) {
             return of('@');
