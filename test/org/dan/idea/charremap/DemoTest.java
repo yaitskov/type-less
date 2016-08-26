ab6be693531ebe0ecd588b8d061e1bc7ef23783d
Daneel Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2016-08-26 16:32:52 +0200

    simple test

---
+++ test/org/dan/idea/charremap/DemoTest.java
@@ -0,0 +1,12 @@
+package org.dan.idea.charremap;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DemoTest {
+    @Test
+    public void stuff() {
+        assertTrue(true);
+    }
+}
