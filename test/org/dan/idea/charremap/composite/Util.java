868a349fd8e486a055f75c473dd044dd8bf4e007
Daneel Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2016-08-26 18:13:55 +0200

    fix util

--- test/org/dan/idea/charremap/composite/Util.java
+++ test/org/dan/idea/charremap/composite/Util.java
@@ -1,17 +1,32 @@
 package org.dan.idea.charremap.composite;
 
+import static com.intellij.psi.TokenType.CODE_FRAGMENT;
+import static com.intellij.psi.TokenType.WHITE_SPACE;
+import static org.junit.Assert.assertEquals;
+
 import com.intellij.psi.tree.IElementType;
+import org.junit.Test;
 
 public class Util {
     public static MockNode seqUp(IElementType... types) {
+        MockNode parent = null;
         MockNode result = null;
         for (IElementType type : types) {
-            MockNode parent = new MockNode(type);
-            if (result != null) {
-                parent.addChild(result);
+            MockNode child = new MockNode(type);
+            if (parent == null) {
+                result = child;
+            } else {
+                child.addChild(parent);
             }
-            result = parent;
+            parent = child;
         }
         return result;
+    }
+
+    @Test
+    public void testSeqUp() {
+        MockNode node = seqUp(WHITE_SPACE, CODE_FRAGMENT);
+        assertEquals(WHITE_SPACE, node.getElementType());
+        assertEquals(CODE_FRAGMENT, node.getTreeParent().getElementType());
     }
 }
