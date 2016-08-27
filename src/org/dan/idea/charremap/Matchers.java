9b6524cdcda90c668cc8a4bc75ebf55e74ddc24a
Daneel Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2016-08-27 16:51:38 +0200

    fix 2=>@ before @

--- src/org/dan/idea/charremap/Matchers.java
+++ src/org/dan/idea/charremap/Matchers.java
@@ -1,6 +1,9 @@
 package org.dan.idea.charremap;
 
+import static com.intellij.psi.JavaTokenType.AT;
+import static com.intellij.psi.TokenType.WHITE_SPACE;
 import static com.intellij.psi.impl.java.stubs.JavaStubElementTypes.JAVA_FILE;
+import static com.intellij.psi.impl.source.tree.JavaElementType.ANNOTATION;
 import static com.intellij.psi.impl.source.tree.JavaElementType.CLASS;
 import static com.intellij.psi.impl.source.tree.JavaElementType.FIELD;
 import static com.intellij.psi.impl.source.tree.JavaElementType.METHOD;
@@ -17,13 +20,21 @@ import static org.dan.idea.charremap.composite.Plus.plus;
 import static org.dan.idea.charremap.composite.PrevChar.prevChar;
 import static org.dan.idea.charremap.composite.Seq.seq;
 
+import org.dan.idea.charremap.composite.Plus;
+
 public class Matchers {
-    public static Matcher AT_MATCHER = seq(WS,
+    private static final Plus P_CLASS = plus(CLASS);
+
+    public static Matcher AT_MATCHER = seq(
             or(
-                    seq(one(FIELD), plus(CLASS)),
-                    seq(maybe(PARAMETER), one(PARAMETER_LIST),
-                            one(METHOD), plus(CLASS)),
-                    seq(maybe(MODIFIER_LIST), any(CLASS))),
+                    seq(one(AT), one(ANNOTATION), one(MODIFIER_LIST),
+                            maybe(FIELD), P_CLASS),
+                    seq(WS,
+                            or(
+                                    seq(one(FIELD), P_CLASS),
+                                    seq(maybe(PARAMETER), one(PARAMETER_LIST),
+                                            one(METHOD), P_CLASS),
+                                    seq(maybe(MODIFIER_LIST), any(CLASS))))),
             one(JAVA_FILE),
             not(prevChar(Character::isJavaIdentifierPart)));
 }
--- test/func/org/dan/idea/charremap/key/AtMatcherTest.java
+++ test/func/org/dan/idea/charremap/key/AtMatcherTest.java
@@ -9,18 +9,87 @@ import org.dan.idea.charremap.Mappers;
 import org.dan.idea.charremap.RootMapper;
 import org.dan.idea.charremap.mock.MockIde;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.LoggerFactory;
 
 public class AtMatcherTest extends LightPlatformCodeInsightFixtureTestCase {
+    static {
+        LoggerFactory.getLogger(AtMatcherTest.class).info("init 12312312");
+    }
+
     public void testTopClassNoAnnotations() {
-        check(1, "\n\nclass A {}\n");
+        checkYes(1, "\n\nclass A {}\n");
     }
 
     public void testTopClassNoAnnotationsFirstChar() {
-        check(0, "\n\nclass A {}\n");
+        checkYes(0, "\n\nclass A {}\n");
     }
 
     public void testTopClassTouchIdAhead() {
-        check('2', '2', 0, "class A {}\n");
+        checkNo(0, "class A {}\n");
+    }
+
+    public void testTopClassTouchIdBack() {
+        checkNo(5, "class A {}\n");
+    }
+
+    public void testTopClassPackage() {
+        checkYes(10, "package x;\n\nclass A {}\n");
+    }
+
+    public void testTopClassImport() {
+        checkYes(25, "import java.lang.String;\n\nclass A {}\n");
+    }
+
+    public void testTopClassPackageAndImport() {
+        checkYes(36, "package x;\nimport java.lang.String;\n\nclass A {}\n");
+    }
+
+    public void testTopClassBeforeAnnotation() {
+        checkYes(0, " @Deprecated\nclass A {}\n");
+    }
+
+    public void testTopClassAfterAnnotation() {
+        checkYes(12, "@Deprecated \nclass A {}\n");
+    }
+
+    public void testTopClassTouchBeforeAnnotation() {
+        checkYes(0, "@Deprecated \nclass A {}\n");
+    }
+
+    public void testFieldTouchBeforeAnnotation() {
+        checkYes(10, "class A { @Deprecated int x; }\n");
+    }
+
+    public void testFieldNoAnnotationAroundSpaces() {
+        checkYes(10, "class A {  int x; }\n");
+    }
+
+    public void testFieldNoAnnotationSpaceAfter() {
+        checkYes(9, "class A { int x; }\n");
+    }
+
+    public void testFieldNoAnnotationTouchType() {
+        checkYes(10, "class A { int x; }\n");
+    }
+
+    public void testMethodNoAnnotationAroundSpaces() {
+        checkYes(10, "class A {  void f() {} }\n");
+    }
+
+    public void testSubClassFieldNoAnnotationTouchTypeAroundSpaces() {
+        checkYes(20, "class A { class B {  int x; } }\n");
+    }
+
+    public void testFieldInAnnotation() {
+        checkNo(11, "class A { @Deprecated int x; }\n");
+    }
+
+    public void testTopClassTouchAfterAnnotation() {
+        checkNo(11, "@Deprecated\nclass A {}\n");
+    }
+
+    public void testTopClassInAnnotation() {
+        checkNo(1, "@Deprecated\nclass A {}\n");
     }
 
     @NotNull
@@ -30,8 +99,12 @@ public class AtMatcherTest extends LightPlatformCodeInsightFixtureTestCase {
         return new MockIde(text, offset, pf.findElementAt(offset).getNode(), c);
     }
 
-    private void check(int offset, String text) {
+    private void checkYes(int offset, String text) {
         check('2', '@', offset, text);
+    }
+
+    private void checkNo(int offset, String text) {
+        check('2', '2', offset, text);
     }
 
     private void check(char in, char out, int offset, String text) {
